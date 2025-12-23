d4e6cc1131c538c8ce0c7cd172edfa018b509219
Trung Nguyen -Kai <trungnguyen1221999@gmail.com>
2025-12-23 19:54:23 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,24 @@ export default defineConfig({
   base: "/",
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1000, 
-    //a
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor-react'
+            }
+            if (id.includes('three') || id.includes('@react-three')) {
+              return 'vendor-three'
+            }
+            if (id.includes('gsap')) {
+              return 'vendor-gsap'
+            }
+            return 'vendor'
+          }
+        }
+      }
+    }
   },
 })
